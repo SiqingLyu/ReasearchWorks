c20b98162d2f8c720d685bb04d01d3f97471931a
Lyu Siqing <47853296+SiqingLyu@users.noreply.github.com>
2024-03-06 14:28:07 +0800

    Add files via upload

--- codes/GetImageFromAll.py
+++ codes/GetImageFromAll.py
@@ -5,7 +5,7 @@
 输入：list[[longitude1, latitude1], [longitude2, latitude2]]  #  左上角和右下角的经纬度坐标，保留至5位小数
 输出：array  # 对应位置的足迹数据
 代码创建时间：2024.02.28
-最后更新时间：2024.02.28
+最后更新时间：2024.03.04
 Lyu Siqing
 """
 
@@ -14,6 +14,14 @@ import os
 import numpy as np
 from geopy.distance import geodesic as gd
 from CheckTheEmpty import CBRA_Checker
+import decimal
+
+
+def subtract_demicals(a, b):
+    decimal_a = decimal.Decimal(a)
+    decimal_b = decimal.Decimal(b)
+    results = decimal_a - decimal_b
+    return results
 
 
 def float_range(start, stop, step):
@@ -28,18 +36,22 @@ def float_range(start, stop, step):
             浮点数列表
 
         例子 Example:
-            >>> print(float_range(3.612, 5.78, 0.22))
+            # >>> print(float_range(3.612, 5.78, 0.22))
             [3.612, 3.832, 4.052, 4.272, 4.492, 4.712, 4.932, 5.152, 5.372]
     '''
-    start_digit = len(str(start))-1-str(start).index(".") # 取开始参数小数位数
+    start_digit = len(str(start))-1-str(start).index(".")  # 取开始参数小数位数
     stop_digit = len(str(stop))-1-str(stop).index(".")    # 取结束参数小数位数
     step_digit = len(str(step))-1-str(step).index(".")    # 取步进参数小数位数
     digit = max(start_digit, stop_digit, step_digit)      # 取小数位最大值
     return [(start*10**digit+i*step*10**digit)/10**digit for i in range(int((stop-start)//step))]  # 是否+1取决于是否要用范围上限那个数
 
 
+def gd_lonlat(lonlat1, lonlat2):
+    return gd((lonlat1[1], lonlat1[0]), (lonlat2[1], lonlat2[0]))
+
+
 class CBRALocator:
-    def __init__(self, file_root: str = '', rectangle_pos: list = None, year:int = 2018):
+    def __init__(self, file_root: str = '', rectangle_pos: list = None, year: int = 2018):
         self.root = file_root
         self.pos = rectangle_pos
         self.img_list = []  # 在不考虑切片临界情况时，此list只有一个路径
@@ -55,7 +67,6 @@ class CBRALocator:
 
         self.longitude_list = float_range(self.longitude_from, self.longitude_to, self.bin)
         self.latitude_list = float_range(self.latitude_from, self.latitude_to, self.bin)
-
 
     def get_pos(self):
         return self.pos
@@ -85,13 +96,17 @@ class CBRALocator:
 
         # TODO: 进一步检查这种计算方式是否合理，尝试使用GDAL等地理库计算offset而不是数学方法,缝隙间可能存在误差
 
-        latitude_index_lt = int((left_top[1] - self.latitude_from) / self.bin)
-        longitude_index_lt = int((left_top[0] - self.longitude_from) / self.bin)
-        latitude_index_rb = int((right_bottom[1] - self.latitude_from) / self.bin)
-        longitude_index_rb = int((right_bottom[0] - self.longitude_from) / self.bin)
+        latitude_index_lt = int((left_top[1] * 10 - self.latitude_from * 10) / (self.bin * 10)) + 1
+        longitude_index_lt = int((left_top[0] * 10 - self.longitude_from * 10) / (self.bin * 10))
+        latitude_index_rb = int((right_bottom[1] * 10 - self.latitude_from * 10) / (self.bin * 10)) + 1
+        longitude_index_rb = int((right_bottom[0] * 10 - self.longitude_from * 10) / (self.bin * 10))
 
-        lat_size = gd(left_top, (left_top[0], right_bottom[1])).m  # 图像的纵向宽度
-        lon_size = gd(right_bottom, (left_top[0], right_bottom[0])).m  # 图像的横向宽度， 以偏赤道的同一纬度为准
+        lat_size = gd_lonlat(left_top, (left_top[0], right_bottom[1])).m  # 图像的纵向宽度
+        # lon_size = gd_lonlat(right_bottom, (left_top[0], right_bottom[1])).m  # 图像的横向宽度， 以偏赤道的同一纬度为准
+        lon_size = gd_lonlat(left_top, (right_bottom[0], left_top[1])).m  # 图像的横向宽度， 以偏赤道的同一纬度为准
+
+        lat_size = int(np.ceil(lat_size / self.resolution))
+        lon_size = int(np.ceil(lon_size / self.resolution))
 
         CBRA_name = []
         img_pos = []
@@ -99,11 +114,15 @@ class CBRALocator:
         CBRA_name_lat = self.latitude_list[latitude_index_lt]
         CBRA_name_lon = self.longitude_list[longitude_index_lt]
         CBRA_name.append(os.path.join(self.root, f'CBRA_{self.year}', f'CBRA_{self.year}_E{CBRA_name_lon}_N{CBRA_name_lat}.tif'))
-        lon_distance = gd(left_top, (CBRA_name_lon, left_top[1])).m  # 计算CBRA左上角到索取范围左上角的横向距离
-        lat_distance = gd(left_top, (left_top[0], CBRA_name_lat)).m  # 计算CBRA左上角到索取范围左上角的纵向距离
+        # lon_distance = gd_lonlat(left_top, (CBRA_name_lon, left_top[1])).m  # 计算CBRA左上角到索取范围左上角的横向距离
+        lon_distance = gd_lonlat((CBRA_name_lon, CBRA_name_lat), (left_top[0], CBRA_name_lat)).m  # 计算CBRA左上角到索取范围左上角的横向距离
+
+        lat_distance = gd_lonlat(left_top, (left_top[0], CBRA_name_lat)).m  # 计算CBRA左上角到索取范围左上角的纵向距离
+
         lat_index_inside = int(np.floor(lat_distance / self.resolution))  # 换算成像素行列数
         lon_index_inside = int(np.floor(lon_distance / self.resolution))  # 换算成像素行列数
-
+        print(left_top[0], CBRA_name_lon, lon_index_inside)
+        print(left_top[1], CBRA_name_lat, lat_index_inside)
         # 接下来定位超过边界的其他图像
         if (latitude_index_rb != latitude_index_lt) | (longitude_index_rb != longitude_index_lt):
             # 只要不同，就找到四个角的对应位置，然后根据情况append进CBRA_name
@@ -136,22 +155,21 @@ class CBRALocator:
                 CBRA_name.append('') # 4
 
                 #  先计算留在左上角图像中的数据大小，再计算新的CBRA左上角到索取范围右上角的距离
-                lon_distance_lt2mid = gd(left_top, (CBRA_name_lon_rt, left_top[1])).m  # 计算CBRA左上角到索取范围边界的横向距离
+                lon_distance_lt2mid = gd_lonlat(left_top, (CBRA_name_lon_rt, left_top[1])).m  # 计算CBRA左上角到索取范围边界的横向距离
                 lon_index_inside_lt2mid = int(np.floor(lon_distance_lt2mid / self.resolution))  # 换算成像素行列数
 
                 image_pixels_y_lt = -1    # -1代表取到对应图像最大值，即取[lon_index_inside： ]
-                image_pixels_x_lt = int(np.ceil(lat_size / self.resolution))
+                image_pixels_x_lt = lat_size
                 img_pos.append([lat_index_inside, image_pixels_x_lt, lon_index_inside, image_pixels_y_lt])
 
                 lat_index_inside_rt = lat_index_inside
                 lon_index_inside_rt = 0
                 image_pixels_y_rt = lon_size - lon_index_inside_lt2mid  # -1代表取到对应图像最大值，即取[lon_index_inside： ]
-                image_pixels_x_rt = int(np.ceil(lat_size / self.resolution))
+                image_pixels_x_rt = lat_size
                 img_pos.append([lat_index_inside_rt, image_pixels_x_rt, lon_index_inside_rt, image_pixels_y_rt])
 
                 img_pos.append([])
                 img_pos.append([])
-
 
             elif longitude_index_rb == longitude_index_lt:  # 左下角
                 self.locate_mode = "LB"
@@ -171,10 +189,10 @@ class CBRALocator:
                 CBRA_name.append('')  # 4
 
                 #  先计算留在左上角图像中的数据大小，再计算新的CBRA左上角到索取范围左下角的距离
-                lat_distance_lt2mid = gd(left_top, (left_top[0], CBRA_name_lat_lb)).m  # 计算CBRA左上角到索取范围边界的纵向距离
+                lat_distance_lt2mid = gd_lonlat(left_top, (left_top[0], CBRA_name_lat_lb)).m  # 计算CBRA左上角到索取范围边界的纵向距离
                 lat_index_inside_lt2mid = int(np.floor(lat_distance_lt2mid / self.resolution))  # 换算成像素行列数
 
-                image_pixels_y_lt = int(np.ceil(lon_size / self.resolution))
+                image_pixels_y_lt = lon_size
                 image_pixels_x_lt = -1  # -1代表取到对应图像最大值，即取[lat_index_inside： ]
                 img_pos.append([lat_index_inside, image_pixels_x_lt, lon_index_inside, image_pixels_y_lt])
 
@@ -182,7 +200,7 @@ class CBRALocator:
 
                 lat_index_inside_lb = 0
                 lon_index_inside_lb = lon_index_inside
-                image_pixels_y_lb = int(np.ceil(lon_size / self.resolution))
+                image_pixels_y_lb = lon_size
                 image_pixels_x_lb = lat_size - lat_index_inside_lt2mid
                 img_pos.append([lat_index_inside_lb, image_pixels_x_lb, lon_index_inside_lb, image_pixels_y_lb])
 
@@ -207,9 +225,9 @@ class CBRALocator:
                 CBRA_name.append(os.path.join(self.root, f'CBRA_{self.year}', f'CBRA_{self.year}_E{CBRA_name_lon_rb}_N{CBRA_name_lat_rb}.tif'))
 
                 #  先计算留在左上角图像中的数据大小
-                lon_distance_lt2mid = gd(left_top, (CBRA_name_lon_rt, left_top[1])).m  # 计算CBRA左上角到索取范围边界的横向距离
+                lon_distance_lt2mid = gd_lonlat(left_top, (CBRA_name_lon_rt, left_top[1])).m  # 计算CBRA左上角到索取范围边界的横向距离
                 lon_index_inside_lt2mid = int(np.floor(lon_distance_lt2mid / self.resolution))  # 换算成像素行列数
-                lat_distance_lt2mid = gd(left_top, (left_top[0], CBRA_name_lat_lb)).m  # 计算CBRA左上角到索取范围边界的纵向距离
+                lat_distance_lt2mid = gd_lonlat(left_top, (left_top[0], CBRA_name_lat_lb)).m  # 计算CBRA左上角到索取范围边界的纵向距离
                 lat_index_inside_lt2mid = int(np.floor(lat_distance_lt2mid / self.resolution))  # 换算成像素行列数
 
                 image_pixels_y_lt = -1  # -1代表取到对应图像最大值，即取[lon_index_inside： ]
@@ -254,8 +272,8 @@ class CBRALocator:
             CBRA_name.append('')  # 3
             CBRA_name.append('')  # 4
             # python读取图像为array导致纬度代表x轴位置、经度代表y轴位置
-            image_pixels_y = int(np.ceil(lon_size / self.resolution))
-            image_pixels_x = int(np.ceil(lat_size / self.resolution))
+            image_pixels_y = lon_size
+            image_pixels_x = lat_size
 
             img_pos.append([lat_index_inside, image_pixels_x, lon_index_inside, image_pixels_y])
             img_pos.append([])  # 2
@@ -274,7 +292,7 @@ class CBRALocator:
         # TODO: 根据提供的影像名称以及坐标位置得到对应的数据
         CBRA_names = self.CBRA_names
         img_pos = self.img_pos
-        img_data = np.zeros((self.lat_size, self.lon_size))
+        img_data = np.zeros((self.lat_size, self.lon_size), dtype=np.uint8)
 
         if self.locate_mode == 'Dome':
             CBRA_data = tif.imread(CBRA_names[0])
@@ -309,6 +327,8 @@ class CBRALocator:
             CBRA_data = tif.imread(CBRA_names[0])
             lat_size_CBRA, lon_size_CBRA = CBRA_data.shape
             lat_index_inside, _, lon_index_inside, _ = img_pos[0]
+            print(img_data.shape, lat_size_CBRA, lat_index_inside, lon_size_CBRA, lon_index_inside)
+
             img_data[0:(lat_size_CBRA - lat_index_inside), 0:(lon_size_CBRA - lon_index_inside)] \
                 = CBRA_data[lat_index_inside:, lon_index_inside:]
 
@@ -326,3 +346,12 @@ class CBRALocator:
             _, image_pixels_x, _, image_pixels_y = img_pos[3]
             img_data[(lat_size_CBRA - lat_index_inside):, (lon_size_CBRA - lon_index_inside):] \
                 = CBRA_data[0: image_pixels_x, 0: image_pixels_y]
+        return img_data
+
+
+if __name__ == '__main__':
+    CL = CBRALocator(file_root=r'G:\ProductData\CBRA', rectangle_pos=[[115.9, 41.4], [116.1, 41.2]])
+    CL.locate_image()
+    print(CL.CBRA_names)
+    img_data = CL.get_img_data()
+    tif.imsave(r'G:\ProductData\CBRA\CBRA_clip\Beijing.tif', img_data)
